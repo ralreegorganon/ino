0fdcaa579d281d9948d9496c4ef397046da758b7
Jason Jones <jasonedwardjones@gmail.com>
2017-07-14 19:14:19 -0800

    Fix whitespace in migration scripts

--- migrations/1499985068_create_table_message.up.sql
+++ migrations/1499985068_create_table_message.up.sql
@@ -1,9 +1,9 @@
 create table message
 (
-      message_id serial not null,
-      mmsi int not null,
-      type int not null,
-      message jsonb not null,
-      created_at timestamp with time zone not null default now(),
-      constraint message_pkey primary key (message_id)
+    message_id serial not null,
+    mmsi int not null,
+    type int not null,
+    message jsonb not null,
+    created_at timestamp with time zone not null default now(),
+    constraint message_pkey primary key (message_id)
 );
